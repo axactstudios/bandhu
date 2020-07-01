2a4bc06674f60700fdb2c8f9fe42ee21d3adf9f9
Chitransh Jain <cjsmcs2810@gmail.com>
2020-07-01 22:35:34 +0530

    added bottom nav bar

---
+++ lib/Screens/MyDocuments.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyDocuments extends StatefulWidget {
+  @override
+  _MyDocumentsState createState() => _MyDocumentsState();
+}
+
+class _MyDocumentsState extends State<MyDocuments> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
