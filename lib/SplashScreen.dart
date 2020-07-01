437c98d56395704b800c5dce5684df5dda35e2b6
Chitransh Jain <cjsmcs2810@gmail.com>
2020-07-01 21:36:14 +0530

    added splash screen

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,46 @@
+import 'package:bandhunew/auth/SignInPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'MyHomePage.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  FirebaseAuth mAuth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    new Future.delayed(Duration(seconds: 3), () async {
+      FirebaseUser user = await mAuth.currentUser();
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MyHomePage()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SignInPage()),
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Image.asset('assets/bandhu.png'),
+        ),
+      ),
+    );
+  }
+}
