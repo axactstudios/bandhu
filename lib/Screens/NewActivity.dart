c938fe94b4643c8ddc9c4c5a7d99d5d9caddeb52
scorchy <vkumarsaraswat@gmail.com>
2020-07-05 22:24:05 +0530

    progress indicator

--- lib/Screens/NewActivity.dart
+++ lib/Screens/NewActivity.dart
@@ -7,7 +7,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:path/path.dart' as p;
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -27,32 +26,12 @@ final FirebaseAuth mAuth = FirebaseAuth.instance;
 String name = "";
 
 class _NewActivityState extends State<NewActivity> {
-//  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-//  String unique;
-//  List<Asset> images = List<Asset>();
-//  Map<String, String> vids = Map<String, String>();
-//  String _error = 'No Error Dectected';
-//  List<String> imageUrls = <String>[];
-//  //List<String> vidUrls = <String>[];
-//  ProgressDialog pr;
-//  bool _isLoading = false;
-//  double _progress = 0;
-//  String _path;
-//  Map<String, String> _paths;
-//
-//  List<StorageUploadTask> _tasks = <StorageUploadTask>[];
-//
-//  Future<void> openFileExplorer() async {
-//    try {
-//      _path = null;
-//
-//      _paths = await FilePicker.getMultiFilePath(type: FileType.any);
-//      vids = _paths;
-//    } on PlatformException catch (e) {
-//      print("Unsupported operation" + e.toString());
-//    }
-//    if (!mounted) return;
-//  }
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  ProgressDialog pr;
+  bool _isLoading = false;
+  double _progress = 0;
+
 
   String unique;
   File file;
@@ -63,7 +42,7 @@ class _NewActivityState extends State<NewActivity> {
   List<String> imageUrls = [];
   List<String> videoUrls = [];
 
-  void _uploadImage(File file, String filename) async {
+  Future<void> _uploadImage(File file, String filename) async {
     final FirebaseStorage _storage =
         FirebaseStorage(storageBucket: 'gs://bandhu-d4b07.appspot.com');
     FirebaseUser user = await FirebaseAuth.instance.currentUser();
@@ -74,6 +53,24 @@ class _NewActivityState extends State<NewActivity> {
 
     final StorageUploadTask uploadTask = storageReference.putFile(file);
     Fluttertoast.showToast(msg: 'Uploading...', gravity: ToastGravity.CENTER);
+    uploadTask.events.listen((event) {
+      setState(() {
+        _isLoading = true;
+        _progress = (event.snapshot.bytesTransferred.toDouble() /
+            event.snapshot.totalByteCount.toDouble()) *
+            100;
+        print('${_progress.toStringAsFixed(2)}%');
+        pr.update(
+          progress: double.parse(_progress.toStringAsFixed(2)),
+          maxProgress: 100.0,
+        );
+      });
+    }).onError((error) {
+      _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        content: new Text(error.toString()),
+        backgroundColor: Colors.red,
+      ));
+    });
     final StorageTaskSnapshot downloadUrl = (await uploadTask.onComplete);
     final String url = (await downloadUrl.ref.getDownloadURL());
     print("URL is $url");
@@ -93,8 +90,27 @@ class _NewActivityState extends State<NewActivity> {
         fileName = p.basename(file.path);
       });
       print(fileName);
+      pr = ProgressDialog(
+        context,
+        type: ProgressDialogType.Download,
+        textDirection: TextDirection.rtl,
+        isDismissible: true,
+      );
+      pr.style(
+        message: 'Uploading photos...',
+        borderRadius: 10.0,
+        backgroundColor: Colors.white,
+        elevation: 10.0,
+        insetAnimCurve: Curves.easeInOut,
+        progressWidgetAlignment: Alignment.center,
+        progressTextStyle: TextStyle(
+            color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
+        messageTextStyle: TextStyle(
+            color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
+      );
+      await pr.show();
 
-      _uploadImage(file, fileName);
+      await _uploadImage(file, fileName);
     } on PlatformException catch (e) {
       showDialog(
           context: context,
@@ -113,7 +129,8 @@ class _NewActivityState extends State<NewActivity> {
             );
           });
     }
-    setState(() {
+    setState(() async {
+      await pr.hide();
       print(fileName);
     });
   }
@@ -129,6 +146,24 @@ class _NewActivityState extends State<NewActivity> {
 
     final StorageUploadTask uploadTask = storageReference.putFile(file);
     Fluttertoast.showToast(msg: 'Uploading...', gravity: ToastGravity.CENTER);
+    uploadTask.events.listen((event) {
+      setState(() {
+        _isLoading = true;
+        _progress = (event.snapshot.bytesTransferred.toDouble() /
+            event.snapshot.totalByteCount.toDouble()) *
+            100;
+        print('${_progress.toStringAsFixed(2)}%');
+        pr.update(
+          progress: double.parse(_progress.toStringAsFixed(2)),
+          maxProgress: 100.0,
+        );
+      });
+    }).onError((error) {
+      _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        content: new Text(error.toString()),
+        backgroundColor: Colors.red,
+      ));
+    });
     final StorageTaskSnapshot downloadUrl = (await uploadTask.onComplete);
     final String url = (await downloadUrl.ref.getDownloadURL());
     print("URL is $url");
@@ -147,7 +182,25 @@ class _NewActivityState extends State<NewActivity> {
         fileName = p.basename(file.path);
       });
       print(fileName);
-
+      pr = ProgressDialog(
+        context,
+        type: ProgressDialogType.Download,
+        textDirection: TextDirection.rtl,
+        isDismissible: true,
+      );
+      pr.style(
+        message: 'Uploading videos...',
+        borderRadius: 10.0,
+        backgroundColor: Colors.white,
+        elevation: 10.0,
+        insetAnimCurve: Curves.easeInOut,
+        progressWidgetAlignment: Alignment.center,
+        progressTextStyle: TextStyle(
+            color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
+        messageTextStyle: TextStyle(
+            color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
+      );
+      await pr.show();
       _uploadVideo(file, fileName);
     } on PlatformException catch (e) {
       showDialog(
@@ -167,225 +220,22 @@ class _NewActivityState extends State<NewActivity> {
             );
           });
     }
-    setState(() {
+    setState(() async {
+      await pr.hide();
       print(fileName);
     });
   }
-
-//  uploadToFirebase(String key) async {
-//    final FirebaseUser user = await mAuth.currentUser();
-//    String uid = user.uid;
-//    pr = ProgressDialog(
-//      context,
-//      type: ProgressDialogType.Download,
-//      textDirection: TextDirection.rtl,
-//      isDismissible: true,
-//    );
-//    pr.style(
-//      message: 'Uploading videos...',
-//      borderRadius: 10.0,
-//      backgroundColor: Colors.white,
-//      elevation: 10.0,
-//      insetAnimCurve: Curves.easeInOut,
-//      progressWidgetAlignment: Alignment.center,
-//      progressTextStyle: TextStyle(
-//          color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
-//      messageTextStyle: TextStyle(
-//          color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
-//    );
-//    await pr.show();
-//    _paths.forEach((fileName, filePath) => {
-//          upload(fileName, filePath, key).then((downloadUrl) async {
-//            final FirebaseStorage _storage =
-//                FirebaseStorage(storageBucket: 'gs://bandhu-d4b07.appspot.com');
-//            StorageReference storageReference;
-//            storageReference =
-//                _storage.ref().child("ActivityVideos/${user.uid}/$key");
-//            final String url = await storageReference.getDownloadURL();
-//            vidUrls.add(url);
-//            if (vidUrls.length == vids.length) {
-//              dbRef
-//                  .child('Activities')
-//                  .child(user.uid)
-//                  .child(key)
-//                  .set({"Vids": vidUrls}).then((_) {
-//                Fluttertoast.showToast(msg: "Successfully Uploaded");
-//
-//                setState(() async {
-//                  await pr.hide();
-//                  vids.clear();
-//                  vidUrls.clear();
-//                });
-//              });
-//            }
-//          })
-//        });
-//  }
-
-//  upload(fileName, filePath, String key) async {
-//    vidUrls.clear();
-//    final FirebaseStorage _storage =
-//        FirebaseStorage(storageBucket: 'gs://bandhu-d4b07.appspot.com');
-//    final FirebaseUser user = await mAuth.currentUser();
-//    String uid = user.uid;
-//    StorageReference storageReference;
-//    storageReference = _storage.ref().child("ActivityVideos/${user.uid}/$key");
-////    _extension = fileName.toString().split('.').last;
-//
-//    final StorageUploadTask uploadTask = storageReference.putFile(
-//      File(filePath),
-//      StorageMetadata(
-//        contentType: 'video/mp4',
-//      ),
-//    );
-//    uploadTask.events.listen((event) {
-//      setState(() {
-//        _isLoading = true;
-//        _progress = (event.snapshot.bytesTransferred.toDouble() /
-//                event.snapshot.totalByteCount.toDouble()) *
-//            100;
-//        print('${_progress.toStringAsFixed(2)}%');
-//        pr.update(
-//          progress: double.parse(_progress.toStringAsFixed(2)),
-//          maxProgress: 100.0,
-//        );
-//      });
-//    }).onError((error) {
-//      _scaffoldKey.currentState.showSnackBar(new SnackBar(
-//        content: new Text(error.toString()),
-//        backgroundColor: Colors.red,
-//      ));
-//    });
-//
-//    setState(() {
-//      print(vidUrls.length);
-//      _tasks.add(uploadTask);
-//    });
-//  }
-
-//  Future<dynamic> postImage(Asset imageFile, String key) async {
-//    final FirebaseStorage _storage =
-//        FirebaseStorage(storageBucket: 'gs://bandhu-d4b07.appspot.com');
-//    final FirebaseUser user = await mAuth.currentUser();
-//    String uid = user.uid;
-//    StorageReference storageReference;
-//    storageReference = _storage.ref().child("ActivityImages/${user.uid}/$key");
-//    StorageUploadTask uploadTask = storageReference
-//        .putData((await imageFile.getByteData()).buffer.asUint8List());
-//    uploadTask.events.listen((event) {
-//      setState(() {
-//        _isLoading = true;
-//        _progress = (event.snapshot.bytesTransferred.toDouble() /
-//                event.snapshot.totalByteCount.toDouble()) *
-//            100;
-//        print('${_progress.toStringAsFixed(2)}%');
-//        pr.update(
-//          progress: double.parse(_progress.toStringAsFixed(2)),
-//          maxProgress: 100.0,
-//        );
-//      });
-//    }).onError((error) {
-//      _scaffoldKey.currentState.showSnackBar(new SnackBar(
-//        content: new Text(error.toString()),
-//        backgroundColor: Colors.red,
-//      ));
-//    });
-//
-//    StorageTaskSnapshot storageTaskSnapshot = await uploadTask.onComplete;
-//    print(storageTaskSnapshot.ref.getDownloadURL());
-//
-//    return storageTaskSnapshot.ref.getDownloadURL();
-//  }
-//
-//  void uploadImages(String key) async {
-//    final FirebaseUser user = await mAuth.currentUser();
-//    String uid = user.uid;
-//    pr = ProgressDialog(
-//      context,
-//      type: ProgressDialogType.Download,
-//      textDirection: TextDirection.rtl,
-//      isDismissible: true,
-//    );
-//    pr.style(
-//      message: 'Uploading photos...',
-//      borderRadius: 10.0,
-//      backgroundColor: Colors.white,
-//      elevation: 10.0,
-//      insetAnimCurve: Curves.easeInOut,
-//      progressWidgetAlignment: Alignment.center,
-//      progressTextStyle: TextStyle(
-//          color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
-//      messageTextStyle: TextStyle(
-//          color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
-//    );
-//    await pr.show();
-//    for (var imageFile in images) {
-//      postImage(imageFile, key).then((downloadUrl) {
-//        imageUrls.add(downloadUrl.toString());
-//        if (imageUrls.length == images.length) {
-//          dbRef
-//              .child('Activities')
-//              .child(user.uid)
-//              .child(key)
-//              .update({"Images": imageUrls}).then((_) {
-//            Fluttertoast.showToast(msg: "Successfully Uploaded");
-//
-//            setState(() {
-//              images.clear();
-//              imageUrls.clear();
-//            });
-//          });
-//        }
-//      });
-//    }
-//    Future.delayed(Duration(seconds: 2)).then((value) {
-//      pr.hide().whenComplete(() {
-//        print(pr.isShowing());
-//      });
-//    });
-//  }
-//
-//  Future<void> loadAssets() async {
-//    List<Asset> resultList = List<Asset>();
-//    String error = 'No Error Dectected';
-//
-//    try {
-//      resultList = await MultiImagePicker.pickImages(
-//        maxImages: 12,
-//        enableCamera: true,
-//        selectedAssets: images,
-//        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
-//        materialOptions: MaterialOptions(
-//          actionBarColor: "#abcdef",
-//          actionBarTitle: "Example App",
-//          allViewTitle: "All Photos",
-//          useDetailsView: false,
-//          selectCircleStrokeColor: "#000000",
-//        ),
-//      );
-//    } on Exception catch (e) {
-//      error = e.toString();
-//    }
-//
-//    // If the widget was removed from the tree while the asynchronous platform
-//    // message was in flight, we want to discard the reply rather than calling
-//    // setState to update our non-existent appearance.
-//    if (!mounted) return;
-//
-//    setState(() {
-//      images = resultList;
-//      _error = error;
-//    });
-//  }
 
   @override
   void initState() {}
 
   @override
   Widget build(BuildContext context) {
+
     int keyLength = randomBetween(5, 15);
     String key = randomAlpha(keyLength);
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(
           'Add new activity',
@@ -521,41 +371,6 @@ class _NewActivityState extends State<NewActivity> {
               SizedBox(
                 height: 20,
               ),
-//              InkWell(
-//                onTap: () async {
-//                  await openFileExplorer();
-//
-//                  await uploadToFirebase(unique);
-//                  setState(() {
-//                    name = activityName.text;
-//                    print(name);
-//                  });
-//                },
-//                child: Container(
-//                  decoration: BoxDecoration(
-//                    color: Color(0xFF6F35A5),
-//                    borderRadius: BorderRadius.circular(33),
-//                  ),
-//                  width: ((MediaQuery.of(context).size).width * 0.8),
-//                  child: Center(
-//                    child: Padding(
-//                      padding: const EdgeInsets.all(8.0),
-//                      child: Text(
-//                        'Upload Videos',
-//                        style: GoogleFonts.poppins(
-//                          textStyle: TextStyle(
-//                              fontSize: 18,
-//                              fontWeight: FontWeight.w400,
-//                              color: Colors.white),
-//                        ),
-//                      ),
-//                    ),
-//                  ),
-//                ),
-//              ),
-//              SizedBox(
-//                height: 20,
-//              ),
               InkWell(
                 onTap: () {
                   setState(() {});
